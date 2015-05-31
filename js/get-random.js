55989e2ebbd6f9c94701d56426a23a887e3393e7
Axel Han <axknightroad@gmail.com>
2015-05-31 22:36:55 +0800

    changelog
    changelog

--- js/get-random.js
+++ js/get-random.js
@@ -59,7 +59,7 @@ function showItem() {
     document.getElementById("items-list").innerHTML=("");
   }
   else {
-    var thead-name ="<tr><th>序号</th><th>候选项</th></tr>"
+    var thead-name ="<tr><th>"+"序号"+"</th><th>"+"候选项"+"</th></tr>"
     var list = "";
     for (var i = 0; i < items.length; i++) {
       var j=i+1;
